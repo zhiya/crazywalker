3033e59523920999a41f0ffa009883b09024cebe
vendar <eplaylity@gmail.com>
2011-11-04 14:53:03 +0800

    add key event handler and messaging between background and content

---
+++ contentscript.js
@@ -0,0 +1,42 @@
+
+document.addEventListener('keydown',function(event){
+	if(event.altKey){
+		switch(event.keyCode){
+			case 49:
+				chrome.extension.sendRequest({
+				"action":"goWalker",
+				"type":"baidubaike",
+				"text":window.getSelection().toString()
+				});
+				break;
+			case 50:
+				chrome.extension.sendRequest({
+				"action":"goWalker",
+				"type":"jinshanciba",
+				"text":window.getSelection().toString()
+				});
+				break;
+			case 51:
+				chrome.extension.sendRequest({
+				"action":"goWalker",
+				"type":"gtranslate",
+				"text":window.getSelection().toString()
+				});
+				break;
+			case 52:
+				chrome.extension.sendRequest({
+				"action":"goWalker",
+				"type":"baidu",
+				"text":window.getSelection().toString()
+				});
+				break;
+			case 53:
+				chrome.extension.sendRequest({
+				"action":"goWalker",
+				"type":"google",
+				"text":window.getSelection().toString()
+				});
+				break;
+		}
+	}
+},true);
